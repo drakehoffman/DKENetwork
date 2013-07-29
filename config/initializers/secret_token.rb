d24e0356ea16775b6918fb9e059ef77a855c5a44
Drake Hoffman <drakephoffman@gmail.com>
2013-07-29 13:42:23 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DKENetwork::Application.config.secret_token = '48c7bccd1c247fa5fa785b8b51e4b59488394d8b645eaabd338702a4ba545e750515c2dedecbfca7cb1f620b12e4eb51e490128108a7321ad66321fb578ed543'
