76e366a34ba3ef168a765d91a19ba04042be21dc
Drake Hoffman <drakephoffman@gmail.com>
2013-08-22 17:42:36 -0400

    Fix Attempt2: Pin Body

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
   attr_accessible :title, :body
 
-  validates :description, presence: true
+  validates :body, presence: true
 
   belongs_to :user
   validates :user_id, presence: true
