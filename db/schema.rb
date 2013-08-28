89bca80ddd4e2cb3d462fece84c01b188ce7126f
Drake Hoffman <drakephoffman@gmail.com>
2013-08-28 12:59:50 -0400

    Paperclip part 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130825002236) do
+ActiveRecord::Schema.define(:version => 20130826190409) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "user_id"
     t.string   "title"
     t.text     "body"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
